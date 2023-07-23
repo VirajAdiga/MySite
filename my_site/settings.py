674a0b43be49ad47525d8b8686507b7ffb316a35
VirajAdiga <virajadigamaclub@gmail.com>
2023-07-23 13:04:18 +0530

    Added env variables instead of hardcoding them

--- my_site/settings.py
+++ my_site/settings.py
@@ -10,20 +10,30 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
+import os
+import environ
+
 from pathlib import Path
+
+env = environ.Env(
+    # set casting, default value
+    DEBUG=(bool, False)
+)
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+# Take environment variables from .env file
+environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = ''
+SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = env('DEBUG')
 
 ALLOWED_HOSTS = []
 
@@ -141,7 +151,7 @@ MEDIA_ROOT = BASE_DIR / "uploads"
 
 MEDIA_URL = 'files/'
 
-redis_url = "redis://127.0.0.1:6379"
+redis_url = f"redis://{env('REDIS_HOST')}:{env('REDIS_PORT')}"
 
 CACHES = {
     'default': {
